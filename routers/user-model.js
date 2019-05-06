6ba349359d020289400ce6c633580beaa3da05c8
Yeshi <tyeshi777@yahoo.com>
2019-05-06 15:21:46 -0700

    completed

--- routers/user-model.js
+++ routers/user-model.js
@@ -11,8 +11,8 @@ function find() {
   return db("users").select("id", "username", "password");
 }
 
-function findBy(id) {
-  return db("users").where(id);
+function findBy(user) {
+  return db("users").where(user);
 }
 async function add(user) {
   const [id] = await db("users").insert(user);
--- routers/user-router.js
+++ routers/user-router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const Users = require("./user-model.js");
 const bcrypt = require("bcryptjs");
-
+const protected = require("../auth/protected-middleware.js");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("It's alive!");
-});
+// router.get("/", (req, res) => {
+//   res.send("It's alive!");
+// });
 
 router.post("/register", (req, res) => {
   let user = req.body;
@@ -22,4 +22,30 @@ router.post("/register", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}` });
+      } else {
+        res.status(401).json({ message: "invalid credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/", protected, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
